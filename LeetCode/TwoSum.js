859976ab40f6c520fd9882246bca2a3fb674b80f
Hussein <112550601+Husseinn-Essam@users.noreply.github.com>
2023-08-26 16:31:24 +0300

    2nd sol twosum

--- LeetCode/TwoSum.js
+++ LeetCode/TwoSum.js
@@ -1,18 +1,25 @@
-// /**
-//  * @param {number[]} nums
-//  * @param {number} target
-//  * @return {number[]}
-//  */
-var twoSum = function (nums, target) {
-  for (i = 0; i < nums.length; i++) {
-    const rem = target - nums[i];
-    for (j = i + 1; j <= nums.length - 1; j++) {
-      if (nums[j] == rem) {
-        return [i, j];
-      }
-    }
-  }
-  return [];
-};
+// O(n^2)
+// var twoSum = function (nums, target) {
+//   for (i = 0; i < nums.length; i++) {
+//     const rem = target - nums[i];
+//     for (j = 0; j <= nums.length - 1; j++) {
+//       if (nums[j] == rem) {
+//         return [i, j];
+//       }
+//     }
+//   }
+//   return [];
+// };
 
+// O(n)
+var twoSum = function (nums, target) {
+  let mp = new Map();
+  for (i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (mp.has(diff)) {
+      return [mp.get(diff), i];
+    }
+    mp.set(nums[i], i);
+  }
+};
 console.log(twoSum([3, 2, 4], 6));
