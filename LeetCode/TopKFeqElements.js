e3eced95e9e8699ae3a699d5fc121a109da5a6df
Hussein <112550601+Husseinn-Essam@users.noreply.github.com>
2023-09-03 14:17:14 +0300

    bucket sort method for TopKFreq

--- LeetCode/TopKFeqElements.js
+++ LeetCode/TopKFeqElements.js
@@ -3,43 +3,53 @@
  * @param {number} k
  * @return {number[]}
  */
+// time : O(n), space: O(n) bucket sort method
 var topKFrequent = function (nums, k) {
-  let repeated = {};
+  let frequency = new Map();
+  let bucket = [];
   let mostFreq = [];
-  nums = nums.sort();
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] == nums[i + 1]) {
-      if (repeated.hasOwnProperty(nums[i])) {
-        repeated[nums[i]]++;
-      } else {
-        repeated[nums[i]] = 2;
-      }
-    } else if (nums[i] != nums[i + 1] && !repeated.hasOwnProperty(nums[i])) {
-      repeated[nums[i]] = 1;
+  nums.forEach((num) => {
+    frequency.set(num, (frequency.get(num) || 0) + 1);
+  });
+  for (let [num, freq] of frequency) {
+    bucket[freq] = (bucket[freq] || new Set()).add(num);
+  }
+  for (let i = bucket.length - 1; i >= 0; i--) {
+    if (k == mostFreq.length) break;
+    if (bucket[i]) {
+      mostFreq.push(...bucket[i]);
     }
   }
-
-  const entries = Object.entries(repeated);
-  entries.sort((a, b) => b[1] - a[1]);
-  for ([key, val] of entries) {
-    if (k === 0) break;
-    mostFreq.push(key);
-    k--;
-  }
-
-  return mostFreq.map((str) => parseInt(str, 10));
+  return mostFreq;
 };
 
-console.log(topKFrequent([1, 1, 1, 2, 2, 3, 3, 3, 4], 2));
-//   let repeatedKeys = [];
-//   let oneTimeKeys = [];
-//   for (const [key, value] of entries) {
-//     if (value > 1) {
-//       repeatedKeys.push(key);
-//     } else {
-//       oneTimeKeys.push(key);
+// time : O(n*logn), space : O(n)
+
+// var topKFrequent = function (nums, k) {
+//   let repeated = {};
+//   let mostFreq = [];
+//   nums = nums.sort();
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] == nums[i + 1]) {
+//       if (repeated.hasOwnProperty(nums[i])) {
+//         repeated[nums[i]]++;
+//       } else {
+//         repeated[nums[i]] = 2;
+//       }
+//     } else if (nums[i] != nums[i + 1] && !repeated.hasOwnProperty(nums[i])) {
+//       repeated[nums[i]] = 1;
 //     }
 //   }
-//   if (repeatedKeys.length == 0)
-//     return oneTimeKeys.map((str) => parseInt(str, 10));
-//   return repeatedKeys.map((str) => parseInt(str, 10));
+
+//   const entries = Object.entries(repeated);
+//   entries.sort((a, b) => b[1] - a[1]);
+//   for ([key, val] of entries) {
+//     if (k === 0) break;
+//     mostFreq.push(key);
+//     k--;
+//   }
+
+//   return mostFreq.map((str) => parseInt(str, 10));
+// };
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3, 3, 3, 4], 2));
