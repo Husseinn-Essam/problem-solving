7fb874326763a57a201b712ebee94379b6528c92
Hussein <112550601+Husseinn-Essam@users.noreply.github.com>
2023-09-06 14:28:37 +0300

    MostWater sol

---
+++ LeetCode/MostWater.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let area;
+  let max = 0;
+  let i = 0;
+  let j = height.length - 1;
+  while (i < j) {
+    area = Math.min(height[i], height[j]) * (j - i);
+    if (area > max) max = area;
+    if (height[i] < height[j]) i++;
+    else j--;
+  }
+  return max;
+};
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
